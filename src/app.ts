037d11923d8f5c073c72af772bfbf179f9b46bd4
Washim-Akram <muhammadwashimakram@gmail.com>
2025-10-05 13:15:56 +0600

    configure environment variables and deploy project

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://wasim-akram.vercel.app",
     credentials: true,
   })
 );
